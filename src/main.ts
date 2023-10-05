425b712a34f7a7bbd7eda8c68cdd5603bec2b2b2
Diego Suntaxi <diego.suntaxi@outlook.es>
2023-10-04 20:27:30 -0500

    feat(config): config module for env variables

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,21 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  app.useGlobalPipes(new ValidationPipe({}));
+
+  // DocumentationService.setup({
+  //   title: 'PEC',
+  //   description: 'PEC API Documentation',
+  //   version: '1.0',
+  //   tag: 'pec',
+  //   app,
+  //   endpointPath: 'api',
+  // });
+  const port = app.get(ConfigService).get('PORT');
+  await app.listen(port);
 }
 bootstrap();
